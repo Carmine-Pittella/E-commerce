a37a3cc286c30da4848c95dccb4c08128ab73a7b
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-08 01:11:43 +0200

    correzione login

--- login.php
+++ login.php
@@ -5,6 +5,13 @@ require "include/dbms.inc.php";
 
 global $connessione;
 
-$main = new Template("skins/template/login.html");
+$main = new Template("skins/template/dtml/index_v2.html");
+$login = new Template("skins/template/login.html");
+
+// tiene aggiornato il numero di oggetti presenti nei preferiti e nel carrello
+require "include/php-utils/preferiti_carrello.php";
+
+
+$main->setContent('body', $login->get());
 $main->close();
 /* non so perché non mi prende i css */
