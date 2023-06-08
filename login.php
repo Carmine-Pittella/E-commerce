8fbb24a582cea9fea22cdb543f1ff03bc6f5e985
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-08 15:38:15 +0200

    ww

--- login.php
+++ login.php
@@ -5,6 +5,17 @@ require "include/dbms.inc.php";
 
 global $connessione;
 
-$main = new Template("skins/template/login.html");
-$main->close();
+/* distinguo due casi 
+1) quando viene chiamata per eseguire l'autenticazine
+2) quando login.php viene chiamata per il display della pagina
+*/
+if(isset($_POST['username'])){
+    /* eseguo l'autenticazione */
+
+}
+else{
+    $main = new Template("skins/template/login.html");
+    $main->close();
 /* non so perché non mi prende i css */
+}
+
