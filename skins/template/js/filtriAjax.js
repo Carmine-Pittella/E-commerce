0451b3eaa9a4407382bb6c5ccdb7d556bb1d799a
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-09 19:20:15 +0200

    query fil

--- skins/template/js/filtriAjax.js
+++ skins/template/js/filtriAjax.js
@@ -1,20 +1,38 @@
 
-    function inviaRichiestaCat(valore) {
-        dataT = {valore : valore, tipo: "categoria"};
-        $.ajax({
-          url: 'shop.php',
-          type: 'POST',
-          data: { valore: dataT},
-          success: function(response) {
-            console.log('successo:',response,'fine');
-            // Puoi fare qualcos'altro con la risposta ottenuta
-            $(document).ready(function(){
-                $("#divItems").html(response)
-            })
-          },
-          error: function(xhr, status, error) {
-            console.log('Si è verificato un errore durante l\'invio della richiesta.');
-          }
-        });
+    function inviaRichiestaFil(arrCategoria,arrGenere,arrMarca,arrPrezzo,size){
+      if(size===undefined){
+        size='U';
+      }
+      if(arrCategoria===undefined){
+        console.log("categoria non definita");
+      }
+      if(arrGenere===undefined){
+        console.log("genere non definito");
+      }
+      if(arrMarca===undefined){
+        console.log("marca non definita");
+      }
+      dataT = {arrCategoria:arrCategoria,
+              arrGenere:arrGenere,
+              arrMarca:arrMarca,
+              arrPrezzo:arrPrezzo,
+              size
+      }
+      $.ajax({
+        url: 'shop.php',
+        type: 'POST',
+        data: { valore: dataT},
+        success: function(response) {
+          console.log('successo:',response,'fine');
+          /*
+          $(document).ready(function(){
+              $("#divItems").html(response)
+          })
+          */
+        },
+        error: function(xhr, status, error) {
+          console.log('Si è verificato un errore durante l\'invio della richiesta.');
+        }
+      });
     }
    
