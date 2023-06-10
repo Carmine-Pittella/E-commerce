88f37ca0b37f43ae08cee31a578fa92499fcdaae
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-10 17:26:08 +0200

    4

--- product.php
+++ product.php
@@ -24,7 +24,12 @@ if (isset($_GET['product_id'])) {
 
 /********* popolamento rullino foto *********/
 $res = $connessione->query("SELECT url_immagine FROM Immagine_Prodotto WHERE id_prodotto = {$product_id};")->fetch_all(MYSQLI_ASSOC);
+$save_first = false;
 foreach ($res as $r) {
+    if (!$save_first) {
+        $body->setContent("FOTO_PRINCIPALE_PRODOTTO", $_GLOBALS['_IMG_PATH'] . $r['url_immagine']);
+        $save_first = true;
+    }
     $rullino_foto = new Template("skins/template/dtml/dtml_items/SequenzaFotoItem.html");
     $rullino_foto->setContent("URL_IMMAGINE", $_GLOBALS['_IMG_PATH'] . $r['url_immagine']);
 }
@@ -35,6 +40,7 @@ $body->setContent("RULLINO_FOTO", $rullino_foto->get());
 /********* popolamento dati Prodotto *********/
 $res = $connessione->query("SELECT * FROM Prodotto p WHERE p.id = {$product_id}")->fetch_all(MYSQLI_ASSOC);
 foreach ($res as $r) {
+    $body->setContent("NOME_PRODOTTO", $r['nome_prodotto']);
     $body->setContent("DESCRIZIONE_PRODOTTO", $r['descrizione']);
 
     // prezzo in promozione
@@ -47,7 +53,14 @@ foreach ($res as $r) {
     } else {
         $body->setContent("PREZZO_PRODOTTO", $r['prezzo']);
     }
+
+    $categoria = $connessione->query("SELECT c.nome_categoria FROM Prodotto p JOIN Categoria c ON {$r['id_categoria']} = c.id")->fetch_all(MYSQLI_ASSOC);
+    $body->setContent("CATEGORIA_PRODOTTO", $categoria[0]['nome_categoria']);
+
+    $marca = $connessione->query("SELECT m.nome_marca FROM Prodotto p JOIN Marca m ON {$r['id_marca']} = m.id")->fetch_all(MYSQLI_ASSOC);
+    $body->setContent("MARCA_PRODOTTO", $marca[0]['nome_marca']);
 }
+
 
 
 
