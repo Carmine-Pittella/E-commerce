5373695d9e781cb6f3a39f2615d122d5b733eca3
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-11 00:33:35 +0200

    rollback effettuato con successo sium

--- skins/template/js/filtriAjax.js
+++ skins/template/js/filtriAjax.js
@@ -1,37 +1,29 @@
-
-    function inviaRichiestaFil(arrCategoria,arrGenere,arrMarca,arrPrezzo,size){
-      if(arrCategoria.length===0){
-        arrCategoria.push(-1);
-      }
-      if(arrGenere.length===0){
-        arrGenere.push(-1);
-      }
-      if(arrMarca.length===0){
-        arrMarca.push(-1);
-      }
-      if(size===undefined){
-        size='U';
-      }
-      dataT = {arrCategoria:arrCategoria,
-              arrGenere:arrGenere,
-              arrMarca:arrMarca,
-              arrPrezzo:arrPrezzo,
-              size
-      }
-      $.ajax({
-        url: 'shop.php',
-        type: 'POST',
-        data: { valore: dataT},
-        success: function(response) {
-          $(document).ready(function(){
-             // $("#divItems").html(response)
-             //console.log(response)
-             $("#divBodyShop").html(response);
-          })
-        },
-        error: function(xhr, status, error) {
-          console.log('Si è verificato un errore durante l\'invio della richiesta.');
-        }
-      });
-    }
-   
+function inviaRichiestaFil(arrCategoria, arrGenere, arrMarca, arrPrezzo, size) {
+   if (arrCategoria.length === 0) {
+      arrCategoria.push(-1);
+   }
+   if (arrGenere.length === 0) {
+      arrGenere.push(-1);
+   }
+   if (arrMarca.length === 0) {
+      arrMarca.push(-1);
+   }
+   if (size === undefined) {
+      size = "U";
+   }
+   dataT = { arrCategoria: arrCategoria, arrGenere: arrGenere, arrMarca: arrMarca, arrPrezzo: arrPrezzo, size };
+   $.ajax({
+      url: "shop.php",
+      type: "POST",
+      data: { valore: dataT },
+      success: function (response) {
+         console.log("successo:", response, "fine");
+         $(document).ready(function () {
+            $("#divItems").html(response);
+         });
+      },
+      error: function (xhr, status, error) {
+         console.log("Si è verificato un errore durante l'invio della richiesta.");
+      },
+   });
+}
--- skins/template/js/scrollAjax.js
+++
@@ -1,6 +0,0 @@
-function swipeRight(arrCategoria,arrGenere,arrMarca,arrPrezzo,size,pageNumber){
-}
-
-
-function swipeLeft(arrCategoria,arrGenere,arrMarca,arrPrezzo,size,pageNumber){
-}
