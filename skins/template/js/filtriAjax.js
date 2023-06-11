bd216bf1158c56b5d67394bbdaed3effa8ece948
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-11 18:13:20 +0200

    admin

--- skins/template/js/filtriAjax.js
+++ skins/template/js/filtriAjax.js
@@ -27,3 +27,33 @@ function inviaRichiestaFil(arrCategoria, arrGenere, arrMarca, arrPrezzo, size) {
       },
    });
 }
+
+function inviaRichiestaFilAdminProduct(arrCategoria, arrGenere, arrMarca, arrPrezzo, size) {
+   if (arrCategoria.length === 0) {
+      arrCategoria.push(-1);
+   }
+   if (arrGenere.length === 0) {
+      arrGenere.push(-1);
+   }
+   if (arrMarca.length === 0) {
+      arrMarca.push(-1);
+   }
+   if (size === undefined) {
+      size = "U";
+   }
+   dataT = { arrCategoria: arrCategoria, arrGenere: arrGenere, arrMarca: arrMarca, arrPrezzo: arrPrezzo, size };
+   $.ajax({
+      url: "admin.php",
+      type: "POST",
+      data: { valore: dataT },
+      success: function (response) {
+         console.log("successo:", response, "fine");
+         $(document).ready(function () {
+            //$("#divItems").html(response);
+         });
+      },
+      error: function (xhr, status, error) {
+         console.log("Si è verificato un errore durante l'invio della richiesta.");
+      },
+   });
+}
