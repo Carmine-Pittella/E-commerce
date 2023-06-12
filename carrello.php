ed46f1cd882898f6023fec75ec83b5f5a232f4d2
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-12 23:01:45 +0200

    fdfdfg

--- carrello.php
+++ carrello.php
@@ -2,6 +2,7 @@
 
 require "include/template2.inc.php";
 require "include/dbms.inc.php";
+require_once "include/php-utils/global.php";
 
 session_start();
 
@@ -9,6 +10,7 @@ global $connessione;
 
 $main = new Template("skins/template/dtml/index_v2.html");
 $body = new Template("skins/template/shopping-cart.html");
+$cart_elem = new Template("skins/template/dtml/dtml_items/shopping-cartItem.html");
 
 // tiene aggiornato il numero di oggetti presenti nei preferiti e nel carrello
 require "include/php-utils/preferiti_carrello.php";
@@ -16,16 +18,41 @@ require "include/php-utils/preferiti_carrello.php";
 
 if (isset($_SESSION['auth']) && $_SESSION['auth']) {
 
-    // prendere tutti gli elementi del carrello con id_utente
     $userid = $_SESSION['utente']['id'];
+    $tot_cart = 0;
 
     $res = $connessione->query("SELECT * FROM Carrello WHERE id_utente = {$userid}");
     foreach ($res as $r) {
         $r['id_prodotto'];
-        $r['quantita'];
+        $r['quantita_prodotto'];
+        // quantita
+        $cart_elem->setContent("QUANTITA", $r['quantita_prodotto']);
+
+        // immagine
+        $tmp = $connessione->query("SELECT url_immagine FROM Immagine_Prodotto WHERE id_prodotto = {$r['id_prodotto']} LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
+        $cart_elem->setContent("URL_IMMAGINE", _IMG_PATH . $tmp[0]['url_immagine']);
+
+        // nome e prezzo
+        $tmp = $connessione->query("SELECT * FROM Prodotto WHERE id = {$r['id_prodotto']};")->fetch_all(MYSQLI_ASSOC);
+        $cart_elem->setContent("NOME_PRODOTTO", $tmp[0]['nome_prodotto']);
+        $cart_elem->setContent("PREZZO_SINGOLO", $tmp[0]['prezzo']);
+
+        // totale prodotto
+        $tot = intval($r['quantita_prodotto']) * floatval($tmp[0]['prezzo']);
+        $cart_elem->setContent("PREZZO_TOTALE", $tot);
+
+        $tot_cart += $tot;
+
+
+
+
+
+        $body->setContent("elemento_carrello", $cart_elem->get());
     }
 
-
+    // totale carrello
+    $body->setContent("TOTALE_PARZIALE_CARRELLO", $tot_cart);
+    $body->setContent("TOTALE_CARRELLO", $tot_cart);
 
 
 
