54070bfcc2af95fdf1825a4d77dad7f498aa8eb3
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-14 00:20:24 +0200

    checkpoint sium

--- carrello.php
+++ carrello.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['auth']) && $_SESSION['auth']) {
         $r['quantita_prodotto'];
         $r['taglia_prodotto'];
 
-        $magazzino = $connessione->query("SELECT * FROM Magazzino WHERE id_prodotto = {$r['id_prodotto']} AND taglia = {$r['taglia_prodotto']} LIMIT 1");
+        $magazzino = $connessione->query("SELECT * FROM Magazzino WHERE id_prodotto = {$r['id_prodotto']} AND taglia = '{$r['taglia_prodotto']}' LIMIT 1")->fetch_all(MYSQLI_ASSOC);;
         if ($magazzino[0]['quantita'] < $r['quantita_prodotto']) {
 
             if ($magazzino[0]['quantita'] == 0) {
@@ -40,35 +40,38 @@ if (isset($_SESSION['auth']) && $_SESSION['auth']) {
         }
     }
 
-    if ($alertNeed != 0) {
-        require_once "include/php-utils/alert.php";
-        Alert::OpenAlert("Le quantità che avevi selezionato per i  tuoi prodotti non sono più disponibili :(");
 
-        switch ($alertNeed) {
-            case 1:
-                // rimuovere il prodotto dal carrello
-                $rmv = $connessione->prepare("DELETE FROM Carrello WHERE id_utente = ? AND id_prodotto = ? AND taglia_prodotto = ?;");
-                $rmv->bind_param("iis", $userid, $r['id_prodotto'], $r['taglia_prodotto']);
-                if ($rmv->execute()) {
-                    echo "Elemento eliminato dal Carrello.";
-                } else {
-                    echo "Errore durante eliminazione in Carrello: " . $rmv->error;
-                }
-                break;
 
-            case 2:
-                // settare la quantità di quel prodotto nel carrello con id_utente = (...) a 1
-                $upd = $connessione->prepare("UPDATE Carrello SET quantita_prodotto = ? WHERE id_prodotto = ? AND id_utente = ? AND taglia_prodotto = ?");
-                $upd->bind_param("iii", $nuova_quantita, $r['id_prodotto'], $userid);
-                if ($upd->execute()) {
-                    echo "Aggiornamento tabella Carrello.";
-                } else {
-                    echo "Errore durante aggiornamento in Carrello: " . $upd->error;
-                }
-                $prod_gia_presente = true;
-                break;
-        }
-    }
+    // QUESTO VA TUTTO ALL'INTERNO DEL FOREACH, ALTRIMENTI COME FA A PRENDERE I PARAMETRI
+    // if ($alertNeed != 0) {
+    //     require_once "include/php-utils/alert.php";
+    //     Alert::OpenAlert("Le quantità che avevi selezionato per i  tuoi prodotti non sono più disponibili :(");
+
+    //     switch ($alertNeed) {
+    //         case 1:
+    //             // rimuovere il prodotto dal carrello
+    //             $rmv = $connessione->prepare("DELETE FROM Carrello WHERE id_utente = ? AND id_prodotto = ? AND taglia_prodotto = ?;");
+    //             $rmv->bind_param("iis", $userid, $r['id_prodotto'], $r['taglia_prodotto']);
+    //             if ($rmv->execute()) {
+    //                 echo "Elemento eliminato dal Carrello.";
+    //             } else {
+    //                 echo "Errore durante eliminazione in Carrello: " . $rmv->error;
+    //             }
+    //             break;
+
+    //         case 2:
+    //             // settare la quantità di quel prodotto nel carrello con id_utente = (...) a 1
+    //             $upd = $connessione->prepare("UPDATE Carrello SET quantita_prodotto = ? WHERE id_prodotto = ? AND id_utente = ? AND taglia_prodotto = ?");
+    //             $upd->bind_param("iis", 1, $r['id_prodotto'], $r['taglia_prodotto']);
+    //             if ($upd->execute()) {
+    //                 echo "Aggiornamento tabella Carrello.";
+    //             } else {
+    //                 echo "Errore durante aggiornamento in Carrello: " . $upd->error;
+    //             }
+    //             break;
+    //     }
+    // }
+    // QUESTO VA TUTTO ALL'INTERNO DEL FOREACH, ALTRIMENTI COME FA A PRENDERE I PARAMETRI
 
 
 
