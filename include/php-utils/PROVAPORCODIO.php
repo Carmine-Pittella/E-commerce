5d3bbfa7a623dd42224d16c0c769a6f427a2906b
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-13 17:23:23 +0200

    lsfdbf

---
+++ include/php-utils/PROVAPORCODIO.php
@@ -0,0 +1,33 @@
+<?php
+
+require "../dbms.inc.php";
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $taglia = $_POST['id_prodotto'];
+
+    echo "il porcodio";
+    echo "<script> console.log('compari diocane') </script>";
+
+    // niente, non va madonna avvelenata dalla peste
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
