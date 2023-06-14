e2d34ea8b29aa1a40a1bed39d061662f494c4862
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-14 11:08:48 +0200

    c1

--- adminPHP/adminCategoria.php
+++ adminPHP/adminCategoria.php
@@ -1,3 +1,9 @@
 <?php
+require "include/template2.inc.php";
+require "include/dbms.inc.php";
+global $connessione;
 
+$res = 
+$items = new Template ("skins/template/dtml/dtml_items/marca_categoriaAdminIteam.html");
 echo "<div>ADMINCATEGORIA</div>";
+
