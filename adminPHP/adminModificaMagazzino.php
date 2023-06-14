0f63cfac41c13e8b21cd8724c162702745758882
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-14 16:52:33 +0200

    inizio query mod magazzino

--- adminPHP/adminModificaMagazzino.php
+++ adminPHP/adminModificaMagazzino.php
@@ -5,5 +5,31 @@ require "../include/template2.inc.php";
 require "../include/dbms.inc.php";
 global $connessione;
 
-$modMagazzino = new Template("../skins/template/adminModificaMagazzino.html");
-$modMagazzino->close();
+
+//chiamata su modifica
+if(isset($_GET['mod'])){
+    $modMagazzino = new Template("../skins/template/adminModificaMagazzino.html");
+    $idItem = $_GET['mod'];
+    $res = $connessione->query("SELECT * FROM Categoria")->fetch_all(MYSQLI_ASSOC);
+    $tot = "";
+    foreach($res as $r){
+        $str = $r['nome_categoria'];
+        $str = '<option value="'.$str.'">'.$str."</option>";
+        $tot = $tot.$str;
+    }
+    $modMagazzino->setContent("select_categoria",$tot);
+    $res = $connessione->query("SELECT * FROM Marca")->fetch_all(MYSQLI_ASSOC);
+    $tot = "";
+    foreach($res as $r){
+        $str = $r['nome_marca'];
+        $str = '<option value="'.$str.'">'.$str."</option>";
+        $tot = $tot.$str;
+    }
+    $modMagazzino->setContent("select_marca",$tot);
+    $modMagazzino->close();
+}
+//chiamata su delete 
+else{
+
+}
+
