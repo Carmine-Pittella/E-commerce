e85f6d18c0147fa159e1974270002dcf42fc33b3
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-16 00:51:34 +0200

    add marche e categoria

--- adminPHP/adminMarche.php
+++ adminPHP/adminMarche.php
@@ -8,21 +8,66 @@ global $connessione;
 
  //display form per la modifica della marca
  if(isset($_REQUEST['mc_id'])){
+    $idC = $_REQUEST['mc_id'];
     $form = new Template("../skins/template/adminModificaCat_Mar.html");
-    $form->setContent("ACTION_FORM","adminMarche");
+    $form->setContent("ACTION_FORM","adminMarche.php");
     $form->setContent("CAT_MAR","marca");
     $form->setContent("NAME_FIELD","formMarca");
     $form->setContent("id_mc",$_REQUEST['mc_id']);
+
+    $res = $connessione->query("SELECT * FROM Marca WHERE id ='$idC' ")->fetch_all(MYSQLI_ASSOC);
+    $form->setContent("DEF_VAL",$res[0]['nome_marca']);
 
     $form ->close();
 
 
 }
+//modifica effettuata 
+if(isset($_POST['formMarca'])){
+  $nome_mar = $_POST['formMarca'];
+  $idC = $_POST['formId'];
+  $connessione->query("UPDATE Marca SET nome_marca = '$nome_mar' WHERE id = '$idC'");
+  header("location:http://localhost/E-commerce/admin.php");
+  exit();
+
+}
+// display aggiungi una nuova marca
+if(isset($_GET['add'])){
+  $add_template = new Template ("../skins/template/adminAggiungiCat_Mar.html");
+  $add_template->setContent("CAT_MAT", "aggiungi una nuova marca");
+  $add_template->setContent("NAME_FIELD","formAddMarca");
+  $add_template->setContent("ACTION_FORM","adminMarche.php");
+  $add_template->setContent("errore","");
+  $add_template->close();
+
+}
+//aggiunta nuova marca
+if(isset($_POST['formAddMarca'])){
+  $nuovaMarca = $_POST['formAddMarca'];
+  $res = $connessione->query("SELECT * FROM Marca WHERE nome_marca = '$nuovaMarca'")->fetch_all(MYSQLI_ASSOC); 
+  if (count($res)===0){
+    $connessione->query("INSERT INTO Marca (nome_marca) VALUES ('$nuovaMarca')");
+    header("location:http://localhost/E-commerce/admin.php");
+    exit();
+  }else{
+    // caso in cui l'elemento già esiste
+    $add_template = new Template ("../skins/template/adminAggiungiCat_Mar.html");
+    $add_template->setContent("CAT_MAT", "aggiungi una nuova marca");
+    $add_template->setContent("NAME_FIELD","formAddMarca");
+    $add_template->setContent("ACTION_FORM","adminMarche.php");
+    $add_template->setContent("errore",' <label style="color: red;">marca già esistente</label>');
+    $add_template->close();
+  }
+
+}
+
+
  
 // display lista marche
-if( !(isset($_REQUEST['mc_id'])) ){
+if( (!isset($_REQUEST['mc_id']))&&(!isset($_POST['NAME_FIELD']))&&(!isset($_GET['add']))&&(!isset($_POST['formAddMarca'])) ){
     $admin_container = new Template("../skins/template/adminContainer2.html");
     $admin_container->setContent('elemento',"una nuova marca");
+    $admin_container->setContent("HREF","adminPHP/adminMarche.php?add=1");
     $res = $connessione->query("SELECT * FROM Marca")->fetch_all(MYSQLI_ASSOC); 
     foreach($res as $r){
         $item = new Template ("../skins/template/dtml/dtml_items/marca_categoriaAdminIteam.html");
@@ -35,16 +80,3 @@ if( !(isset($_REQUEST['mc_id'])) ){
 }
 
 
-
-
-/*
-<div class="container">
-        <form method="post" action="<[ACTION_FORM]>">
-          <div class="form-group">
-            <label for="ttt"><[CAT_MAR]></label>
-            <input type="text" class="form-control"  name="<[NAME_FIELD]>">
-          </div>
-          <button type="submit" class="btn btn-primary">Invia</button>
-        </form>
-    </div>
-*/
