dce0e08211d46f49b70ead1305301371bdd8b57c
Carmine-Pittella <carmine.pittella@student.univaq.it>
2023-06-18 22:53:06 +0200

    promozione homepage

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require "include/template2.inc.php";
 require "include/dbms.inc.php";
+require_once "include/php-utils/global.php";
 
 session_start();
 
@@ -15,19 +16,26 @@ require "include/php-utils/preferiti_carrello.php";
 
 // search id promozione
 $res = $connessione->query("SELECT id_promozione, id_categoria FROM Prodotto WHERE id_promozione IS NOT NULL LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
-$idPromo = $res[0]['id_promozione'];
-$idCateg = $res[0]['id_categoria'];
+if (!empty($res)) {
 
-// promozione
-$res = $connessione->query("SELECT * FROM Promozione WHERE id = {$idPromo} LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
-$res[0]['nome_promozione'];
-$res[0]['descrizione'];
-$res[0]['sconto_percentuale'];
-$res[0]['data_fine'];
+    // categoria associata
+    $cat = $connessione->query("SELECT nome_categoria FROM Categoria WHERE id = {$res[0]['id_categoria']} LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
+    $body->setContent("CATEGORIA_PROMOZIONE", $cat[0]['nome_categoria']);
 
-// categoria associata
-$res = $connessione->query("SELECT nome_categoria FROM Categoria WHERE id = {$idCateg} LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
-$res[0]['nome_categoria'];
+    // promozione
+    $promo = $connessione->query("SELECT * FROM Promozione WHERE id = {$res[0]['id_promozione']} LIMIT 1;")->fetch_all(MYSQLI_ASSOC);
+    $body->setContent("NOME_PROMOZIONE", $promo[0]['nome_promozione']);
+    $body->setContent("DESCRIZIONE_PROMOZIONE", $promo[0]['descrizione']);
+    $body->setContent("SCONTO_PERCENTUALE", $promo[0]['sconto_percentuale']);
+
+    $data = strtotime($promo[0]['data_fine']);
+    $body->setContent("GG", date("d", $data));
+    $body->setContent("MM", date("m", $data));
+    $body->setContent("AAAA", date("Y", $data));
+}
+
+
+
 
 
 
