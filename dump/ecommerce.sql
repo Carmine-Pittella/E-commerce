5dc7c41f51348425cdc12bb5c1db590d5749e20c
fabrizpag <fabrizio.paglia@student.univaq.it>
2023-06-24 18:32:00 +0200

    upd db

--- dump/ecommerce.sql
+++ dump/ecommerce.sql
@@ -207,31 +207,9 @@ CREATE TABLE IF NOT EXISTS User_has_ugroup (
 
 
 
--- --------------------------------  TRIGGER ----------------------------------
-
-
-
-
-
-
-
-
-
--- --------------------------------  PROCEDURE ----------------------------------
-
-
-
-
-
-
-
-
-
-
-
 -- --------------------------------  INSERIMENTO DATI ----------------------------------
 
--- Inserimento di valori casuali nella tabella Utente
+-- Inserimento di valori nella tabella Utente
 INSERT INTO Utente (nome, cognome, email, password)
 VALUES
 ('Mario', 'Rossi', 'mario.rossi@example.com', 'password123'),
@@ -240,7 +218,7 @@ VALUES
 ('Martina', 'Gialli', 'martina.gialli@example.com', '123456'),
 ('Simone', 'Neri', 'simone.neri@example.com', 'password456');
 
--- Inserimento di valori casuali nella tabella Categoria
+-- Inserimento di valori nella tabella Categoria
 INSERT INTO Categoria (nome_categoria)
 VALUES
 ('T-shirt'),
@@ -250,7 +228,7 @@ VALUES
 ('gonne'),
 ('camice');
 
--- Inserimento di valori casuali nella tabella Marca
+-- Inserimento di valori nella tabella Marca
 INSERT INTO Marca (nome_marca)
 VALUES
 ('Nike'),
@@ -263,7 +241,7 @@ VALUES
 ('Stradivarius');
 
 
--- Inserimento di valori casuali nella tabella Promozione
+-- Inserimento di valori nella tabella Promozione
 INSERT INTO Promozione (nome_promozione, descrizione, sconto_percentuale, data_inizio, data_fine)
 VALUES
 ('Sconto Estivo', 'Sconto del 20% su tutti gli articoli estivi', 20.00, '2023-06-01', '2023-06-30'),
@@ -272,7 +250,7 @@ VALUES
 ('Promo Giugno', 'Sconto del 25% su tutti gli articoli', 25.00, '2023-06-01', '2023-06-30'),
 ('Promo Nuovi Arrivi', 'Sconto del 10% su nuovi arrivi', 10.00, '2023-06-01', '2023-06-30');
 
--- Inserimento di valori casuali nella tabella Prodotto
+-- Inserimento di valori nella tabella Prodotto
 INSERT INTO Prodotto (nome_prodotto, descrizione, prezzo, genere, id_categoria, id_marca, id_promozione)
 VALUES
 ('T-shirt', 'descrizione bla bla', 29.99, 'uomo', 1, 4, NULL),
@@ -288,9 +266,12 @@ VALUES
 ('pantalone', 'descrizione bla bla', 199.99, 'uomo', 3, 5, NULL),
 ('camicia', 'descrizione bla bla', 44.99, 'uomo', 6, 5, NULL),
 ('camicia semplice', 'descrizione bla bla', 44.99, 'donna', 6, 8, NULL),
-('camicia semplice', 'descrizione bla bla', 44.99, 'donna', 6, 8, NULL); 
+('camicia semplice', 'descrizione bla bla', 44.99, 'donna', 6, 8, NULL),
+('gonna grigia','descrizione bla bla', 50.77,'donna',5,8, NULL),
+('maglietta bambino','descrizione bla bla',19.99,'bambino',1,8,NULL),
+('maglietta bambino','descrizione bla bla',19.99,'bambino',1,8,NULL);
 
--- Inserimento di valori casuali nella tabella Corriere
+-- Inserimento di valori nella tabella Corriere
 INSERT INTO Corriere (prezzo, azienda, giorni_consegna)
 VALUES
 (5.99, 'CorriereExpress', 7),
@@ -299,7 +280,7 @@ VALUES
 (7.99, 'QuickShip', 8),
 (4.99, 'LocalCourier', 2);
 
--- Inserimento di valori casuali nella tabella Coupon
+-- Inserimento di valori nella tabella Coupon
 INSERT INTO Coupon (codice_coupon, sconto_percentuale)
 VALUES
 ('SUMMER2023', 10.00),
@@ -308,7 +289,7 @@ VALUES
 ('WELCOME10', 10.00),
 ('FLASH50', 50.00);
 
--- Inserimento di valori casuali nella tabella Metodo_Pagamento
+-- Inserimento di valori nella tabella Metodo_Pagamento
 INSERT INTO Metodo_Pagamento (tipo_pagamento)
 VALUES
 ('Carta di Credito'),
@@ -317,7 +298,7 @@ VALUES
 ('Google Pay'),
 ('Bonifico Bancario');
 
--- Inserimento di valori casuali nella tabella Indirizzo_Spedizione
+-- Inserimento di valori nella tabella Indirizzo_Spedizione
 INSERT INTO Indirizzo_Spedizione (id_utente, indirizzo, citta, regione, provincia, CAP)
 VALUES
 (1, 'Via Roma 1', 'Milano', 'Lombardia', 'MI', '20100'),
@@ -326,7 +307,7 @@ VALUES
 (4, 'Rue de la Paix 3', 'Parigi', 'Ile-de-France', '75', '75001'),
 (5, 'Broadway 100', 'New York', 'New York', 'NY', '10001');
 
--- Inserimento di valori casuali nella tabella Ordine
+-- Inserimento di valori nella tabella Ordine
 INSERT INTO Ordine (id_utente, id_corriere, id_coupon, id_metodo_pagamento, id_indirizzo_spedizione, data_ordine, data_spedizione, prezzo_ordine)
 VALUES
 (1, 1, NULL, 1, 1, '2023-06-05', '2023-06-07', 49.99),
@@ -335,7 +316,7 @@ VALUES
 (4, 4, 3, 2, 1, '2023-06-07', NULL, 999.99),
 (5, 1, NULL, 5, 1, '2023-06-07', '2023-06-09', 29.99);
 
--- Inserimento di valori casuali nella tabella Oggetto_Ordine
+-- Inserimento di valori nella tabella Oggetto_Ordine
 INSERT INTO Oggetto_Ordine (id_ordine, id_prodotto, quantita_prodotto)
 VALUES
 (1, 3, 2),
@@ -344,7 +325,7 @@ VALUES
 (4, 1, 1),
 (5, 5, 3);
 
--- Inserimento di valori casuali nella tabella Carrello
+-- Inserimento di valori nella tabella Carrello
 INSERT INTO Carrello (id_utente, id_prodotto, quantita_prodotto, taglia_prodotto)
 VALUES
 (1, 1, 1, 'S'),
@@ -353,16 +334,16 @@ VALUES
 (4, 4, 1, 'L'),
 (5, 5, 2, 'XL');
 
--- Inserimento di valori casuali nella tabella Recensione
+-- Inserimento di valori nella tabella Recensione
 INSERT INTO Recensione (id_utente, id_prodotto, testo_recensione, valutazione, data_recensione)
 VALUES
 (1, 1, 'Ottima maglia, tessuto di alta qualità', 5, '2023-06-07'),
-(2, 3, 'La borsa è perfetta per ogni occasione', 4, '2023-06-01'),
-(3, 2, 'Scarpe comode e di ottima fattura', 4, '2023-05-11'),
-(4, 4, 'iPhone 12 è uno smartphone eccezionale', 5, '2023-06-04'),
-(5, 5, 'Il rossetto ha una lunga durata', 4, '2023-04-12');
+(2, 3, 'ottimo', 4, '2023-06-01'),
+(3, 2, ' di ottima fattura', 4, '2023-05-11'),
+(4, 4, ' eccezionale', 5, '2023-06-04'),
+(5, 5, 'è un buon prodotto', 4, '2023-04-12');
 
--- Inserimento di valori casuali nella tabella Immagine_Prodotto
+-- Inserimento di valori nella tabella Immagine_Prodotto
 INSERT INTO Immagine_Prodotto (id_prodotto, url_immagine)
 VALUES
 (1, 'products/M-element-Tshirt-front-black.jpg'),
@@ -382,10 +363,12 @@ VALUES
 (13, 'products/D-Stradivarius-camicia-front-green.jpg'),
 (13, 'products/D-Stradivarius-camicia-back-green.jpg'),
 (14, 'products/D-Stradivarius-camicia2-front-lightBlu.jpg'),
-(14, 'products/D-Stradivarius-camicia2-back-lightBlu.jpg');
+(14, 'products/D-Stradivarius-camicia2-back-lightBlu.jpg'), 
+(15, 'products/D-Stradivarius-gonna-front-grey.jpg'),
+(16, 'products/B-Stradivarius-Tshirt-front-lightBlu.jpg'),
+(17, 'products/B-Stradivarius-Tshirt2-front-black.jpg');
 
-
--- Inserimento di valori casuali nella tabella Magazzino
+-- Inserimento di valori nella tabella Magazzino
 INSERT INTO Magazzino (id_prodotto, quantita, taglia)
 VALUES
 (1, 10, 'S'),
@@ -439,12 +422,22 @@ VALUES
 (14, 30, 'S'),
 (14, 30, 'M'),
 (14, 30, 'L'),
-(14, 30, 'XL');
+(14, 30, 'XL'),
+(15, 30, 'S'),
+(15, 30, 'M'),
+(15, 30, 'L'),
+(15, 30, 'XL'),
+(16, 30, 'S'),
+(16, 30, 'M'),
+(16, 30, 'L'),
+(16, 30, 'XL'),
+(17, 30, 'S'),
+(17, 30, 'M'),
+(17, 30, 'L'),
+(17, 30, 'XL');
 
 
 -- Inserimento di valori nella tabella Messaggio_Assistenza
--- utente con id1 ('Mario', 'Rossi', 'mario.rossi@example.com', 'password123'),
--- utente con id2 ('Giulia', 'Bianchi', 'giulia.bianchi@example.com', 'pass1234'),
 INSERT INTO Messaggio_Assistenza (id,id_utente,richiesta,risposta, data_richiesta) VALUES
 (1,1,'quando sarà disponibile la prossima promozione?','la prossima promozione sarà una promozione sui prodotti estivi e sarà disponibile fra 3 mesi', '2023-06-07'),
 (2,1,'saranno mai venduti occhiali da sole?',NULL, '2023-06-07'),
@@ -452,36 +445,28 @@ INSERT INTO Messaggio_Assistenza (id,id_utente,richiesta,risposta, data_richiest
 (4,2,'ho eseguito un ordine 3 mesi fa, è possibile eseuguire un reso?',NULL, '2023-06-07'),
 (5,2,'ho appena eseguito un ordine, è possibile annullarlo?',NULL, '2023-06-07');
 
--- Inserimento di valori casuali nella tabella Service
+-- Inserimento di valori nella tabella Service
 INSERT INTO Service (id, script, description) VALUES
 (1, 'admin.php', NULL),
 (2, 'profile.php', NULL);
 
--- Inserimento di valori casuali nella tabella Ugroup
+-- Inserimento di valori nella tabella Ugroup
 INSERT INTO Ugroup (id, name, description) VALUES
 (1, 'Administrator', NULL),
 (2, 'User', NULL);
 
--- Inserimento di valori casuali nella tabella Ugroup_has_service
+-- Inserimento di valori nella tabella Ugroup_has_service
 INSERT INTO Ugroup_has_service (ugroup_id, service_id) VALUES
 (1, 1),
 (2, 2);
 
--- Inserimento di valori casuali nella tabella User_has_ugroup
+-- Inserimento di valori nella tabella User_has_ugroup
 INSERT INTO User_has_ugroup (id, id_utente, id_ugroup) VALUES
 (1, 1, 1),
 (2, 2, 2),
 (3, 3, 2),
 (4, 4, 2),
 (5, 5, 2);
-
-/*
-('Mario', 'Rossi', 'mario.rossi@example.com', 'password123'), ADMIN
-('Giulia', 'Bianchi', 'giulia.bianchi@example.com', 'pass1234'), USER
-('Luca', 'Verdi', 'luca.verdi@example.com', 'securepass'), USER
-('Martina', 'Gialli', 'martina.gialli@example.com', '123456'), USER
-('Simone', 'Neri', 'simone.neri@example.com', 'password456'); USER
-*/
 
 
 
